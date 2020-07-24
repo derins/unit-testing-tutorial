73ec585827b7826625988ed04e78feecb749c534
Derin Serbetcioglu <serbetd@gmail.com>
2020-07-23 23:11:18 -0700

    better tests and testable code working / bad test and untestable code are commented out

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# unit-testing-tutorial
+### Creating the env - ensure you are running the anaconda `4.5.x +`
+```
+conda env create -f environment_unit_test.yml
+```
+
+### Updating the env after adding new packages
+```
+conda env update -f environment_unit_test.yml
+```
+
+### Starting the env
+```
+conda activate unit_test_env
+```
+
+### Stopping the env
+```
+conda deactivate
+```
+
+### Running Locally - ensure you are using python `3.x +`
+```
+python app.py
+```
---
+++ unit_tests.py
@@ -0,0 +1,57 @@
+import unittest
+from time_of_day import *
+from datetime import datetime
+
+# Bad example of test to test poorly writtern testable code
+
+
+# class Test(unittest.TestCase):
+#
+#     # Setup: change system time to 6AM
+#
+#     def test(self):
+#
+#         time_of_day = get_time_of_day()
+#
+#         self.assertEqual("Morning", time_of_day)
+
+
+
+
+# Good examples of test for testable code
+
+
+class Test(unittest.TestCase):
+
+
+    def test_morning(self):
+
+        time_of_day = get_time_of_day(datetime(2020, 7, 15, 6, 00, 00))
+
+        self.assertEqual("Morning", time_of_day)
+
+
+    def test_afternoon(self):
+
+        time_of_day = get_time_of_day(datetime(2020, 7, 15, 14, 12, 54))
+
+        self.assertEqual("Afternoon", time_of_day)
+
+
+    def test_evening(self):
+
+        time_of_day = get_time_of_day(datetime(2029, 7, 15, 23, 59, 59))
+
+        self.assertEqual("Evening", time_of_day)
+
+
+    def test_night(self):
+
+        time_of_day = get_time_of_day(datetime(2020, 7, 15, 2, 30, 00))
+
+        self.assertEqual("Night", time_of_day)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
